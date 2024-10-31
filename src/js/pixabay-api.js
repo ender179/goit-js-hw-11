e43f80a2af81169d66748b9c01f64321f6db982b
ender179 <kirilukvlad91@gmail.com>
2024-10-31 14:42:15 +0200

    Initial commit

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,18 +1,18 @@
-// pixabay-api.js  
-const API_KEY = 'YOUR_API_KEY'; // Замініть на свій ключ  
+require('dotenv').config(); // Подключаем dotenv, если это Node.js
+const API_KEY = process.env.PIXABAY_API_KEY;
 
-export const fetchImages = async (query) => {  
-const url = `https://pixabay.com/api/?key=${46813700-d54068e337851aebc3c875554}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`;  
-    
-try {  
-const response = await fetch(url);  
-if (!response.ok) {  
-            throw new Error('Network response was not ok');  
-}  
-const data = await response.json();  
-return data.hits; // Повертаємо масив зображень  
-} catch (error) {  
-console.error('Fetch error:', error);  
-throw error;  
-}  
+export const fetchImages = async (query) => {
+    const url = `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data.hits; // Возвращаем массив изображений
+    } catch (error) {
+        console.error('Fetch error:', error);
+        throw error;
+    }
 };
