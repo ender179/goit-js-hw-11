bf5066eb2e2fdc5e8965a1b9c3c59cd43ba288d1
ender179 <kirilukvlad91@gmail.com>
2024-10-31 16:26:45 +0200

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // main.js  
-import { fetchImages } from './src/js/pixabay-api.js';  
-import { renderImages, showAlert } from './src/js/render-functions.js';  
+import { fetchImages } from './js/pixabay-api.js';  
+import { renderImages, showAlert } from './js/render-functions.js';  
 import iziToast from 'izitoast';  
 import 'izitoast/dist/css/iziToast.min.css';  
 import SimpleLightbox from 'simplelightbox';  
