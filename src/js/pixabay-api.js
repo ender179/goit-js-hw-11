e8904232e36c68e19ae9ff5fea3c49521d9d9bec
ender179 <kirilukvlad91@gmail.com>
2024-10-31 14:06:27 +0200

    Initial commit

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,17 +2,17 @@
 const API_KEY = 'YOUR_API_KEY'; // Замініть на свій ключ  
 
 export const fetchImages = async (query) => {  
-    const url = `https://pixabay.com/api/?key=${46813700-d54068e337851aebc3c875554}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`;  
+const url = `https://pixabay.com/api/?key=${46813700-d54068e337851aebc3c875554}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`;  
     
-    try {  
-        const response = await fetch(url);  
-        if (!response.ok) {  
+try {  
+const response = await fetch(url);  
+if (!response.ok) {  
             throw new Error('Network response was not ok');  
-        }  
-        const data = await response.json();  
-        return data.hits; // Повертаємо масив зображень  
-    } catch (error) {  
-        console.error('Fetch error:', error);  
-        throw error;  
-    }  
+}  
+const data = await response.json();  
+return data.hits; // Повертаємо масив зображень  
+} catch (error) {  
+console.error('Fetch error:', error);  
+throw error;  
+}  
 };
